6feb0c8b80540d43470479e2e22b76f5526019a7
Andre Askarinam <aaskari96@gmail.com>
2015-01-31 17:50:32 -0800

    Modified hw2 - split tests

--- hw2/CalculatorTest.java
+++ hw2/CalculatorTest.java
@@ -15,28 +15,60 @@ public class CalculatorTest {
      **/
     @Before
     public void setUp() {
-        // tester = new StaffCalculator(); // Comment me out to test your Calculator
-        tester = new Calculator();   // Un-comment me to test your Calculator
+        tester = new StaffCalculator(); // Comment me out to test your Calculator
+        // tester = new Calculator();   // Un-comment me to test your Calculator
     }
 
     // TASK 1: WRITE JUNIT TESTS
     // YOUR CODE HERE -- DONE
 
     @Test
-    public void testAdd() {
+    public void testAdd1() {
         assertEquals(6, tester.add(0, 6));
+    }
+
+    @Test
+    public void testAdd2() {
         assertEquals(11, tester.add(5, 6));
+    }
+
+    @Test
+    public void testAdd3() {
         assertEquals(-4, tester.add(-10, 6));
+    }
+
+    @Test
+    public void testAdd4() {
         assertEquals(53, tester.add(54, -1));
+    }
+
+    @Test
+    public void testAdd5() {
         assertEquals(-26, tester.add(-20, -6));
     }
 
     @Test
-    public void testMultiply() {
+    public void testMultiply1() {
         assertEquals(0, tester.multiply(0, 6));
+    }
+
+    @Test
+    public void testMultiply2() {
         assertEquals(30, tester.multiply(5, 6));
+    }
+
+    @Test
+    public void testMultiply3() {
         assertEquals(-60, tester.multiply(10, -6));
+    }
+
+    @Test
+    public void testMultiply4() {
         assertEquals(-54, tester.multiply(54, -1));
+    }
+
+    @Test
+    public void testMultiply5() {
         assertEquals(120, tester.multiply(-20, -6));
     }
 

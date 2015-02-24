43c65f598d521225efeaa4a3bb981e3a40100dc3
Andre Askarinam <aaskari96@gmail.com>
2015-02-23 19:11:41 -0800

    Finished hw4

--- hw4/synthesizer/GuitarString.java
+++ hw4/synthesizer/GuitarString.java
@@ -1,5 +1,5 @@
 // Make sure to make this class a part of the synthesizer package
-//package <package name>;
+package synthesizer;
 
 //Make sure this class is public
 public class GuitarString {
@@ -18,6 +18,7 @@ public class GuitarString {
         //       cast the result of this divsion operation into an int. For better
         //       accuracy, use the Math.round() function before casting.
         //       Your buffer should be initially filled with zeros.
+        buffer = new ArrayRingBuffer((int) Math.round(SR / frequency));
     }
     
     
@@ -28,6 +29,13 @@ public class GuitarString {
         //       double r = Math.random() - 0.5;
         //
         //       Make sure that your random numbers are different from each other.
+        while (!buffer.isEmpty()) {
+            buffer.dequeue();
+        }
+        for (int i = 0; i < buffer.capacity(); i++) {
+            double r = Math.random() - 0.5;
+            buffer.enqueue(r);
+        }
     }
     
     /* Advance the simulation one time step by performing one iteration of
@@ -37,11 +45,12 @@ public class GuitarString {
         // TODO: Dequeue the front sample and enqueue a new sample that is
         //       the average of the two multiplied by the DECAY factor.
         //       Do not call StdAudio.play().
+        buffer.enqueue(DECAY * 0.5 * (buffer.dequeue() + buffer.peek()));
     }
     
     /* Return the double at the front of the buffer. */
     public double sample() {
-        return 0;
+        return buffer.peek();
     }
     
 }
